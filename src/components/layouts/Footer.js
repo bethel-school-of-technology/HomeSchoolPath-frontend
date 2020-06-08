7c2697f7d73a4fa3b703c9dad27e5eeef8c7b27c
Milton Lash <iblash@live.com>
2020-06-07 21:23:45 -0500

    created header, navbar and footer files

---
+++ src/components/layouts/Footer.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Footer = () => {
+    return (
+        <FooterContainer>
+
+          <span style={{color: "#fff", top: "1.5rem", left: "1rem", position: "relative" }}>
+            &copy; {new Date().getFullYear()}All Rights Reserved. Kingdom Coders.
+          </span>
+            
+        </FooterContainer>
+    )    
+    
+};
+
+export default Footer;
+
+// footer container
+const FooterContainer = styled.footer`
+  background: #344;
+  height: 4rem;
+  position: fixed;
+  left: 0;
+  bottom: 0;
+  width:100%;
+`;
---
+++ src/components/layouts/Navbar.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import styled from 'styled-components';
+import logo from '../../homeschoolpath-logo.png';
+
+const Navbar = () => {
+    return (
+        <NavbarContainer>
+          <nav className="navbar navbar-expand-lg navbar-light px-5 py-0">
+            <a className="navbar-brand" href="#">
+              <img style={{ width: '50px' }} src={logo} alt="logo" />
+        </a>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+      
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav ml-auto">
+            <li className="nav-item active">
+              <a className="nav-link" href="#">
+                Home <span className="sr-only">(current)</span>
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Link
+              </a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+        </NavbarContainer>
+    )
+};
+
+export default Navbar
+
+// main Navbar container
+const NavbarContainer = styled.div`
+background: var(--yellow);
+.nav-link {
+    color: #fff !important;
+    &:hover {
+        background: var(--light-green);
+    }
+}
+`; 
